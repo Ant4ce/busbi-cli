2f2f5408c90b86abbc67299ce957c2d34abb240f
Wessel_Weernink <wesselweernink@gmail.com>
2026-02-24 04:11:37 +0100

    Adds improved parsing, to be able to process a new flag '-x'. x flag allows for making the file executable on either windows or unix system. Needs further improvements to adapt to OS better if user creating d-script is on different system than target OS.

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,14 @@ fn main() -> io::Result<()> {
     //let target_file : &str = &args[2];
     //let desired_file_name : &str = &args[3];
 
-    let Ok((target_os, source_file, destination_file)) = parse_args(&args) else { panic!("Err: Wrong usage, try again.")};
+    //let Ok((target_os, source_file, destination_file)) = parse_args(&args) else { panic!("Err: Wrong usage, try again.")};
+    let (target_os, execute, source_file, destination_file) = match parse_args_advanced(&args) {
+        Ok(x) => x,
+        Err(x) => match x {
+            HelpMessage::MissingFlagO => panic!("Got wrong or missing argument to -o flag."),
+            _ => panic!("Got another error. Check usage."),
+        }
+    };
 
     //println!("the OS target is {target_os}, for target file {source_file} which will be turned into txt file with name {destination_file}");
     let new_file = File::create(destination_file)?;
@@ -37,17 +44,61 @@ fn main() -> io::Result<()> {
 
         }
     } else {
-        println!("Err: File you specified doesn't exist. Your file: {}", source_file);
+        println!("Err: File you specified doesn't exist or something else went wrong. Your file: {}", source_file);
     }
 
     let end_boiler: String = end_boilerplate(target_os, source_file);
     write_buf.write(end_boiler.as_bytes());
+
+    if execute == true {
+        let exec_string = match executable_boiler(target_os, source_file) {
+            Ok(x) => x,
+            Err(x) => panic!("Got an error: {:?}", x),
+        };
+        write_buf.write(exec_string.as_bytes());
+    }
     //This pushes the contents of the buffer to the file. 
     write_buf.flush()?;
     Ok(())
 
 }
 
+fn executable_boiler(os_type: &str, source_file: &str) -> Result<String, HelpMessage> {
+    let mut execute_string : String = String::new();
+
+    //TODO improve this here. Currently we just take the source file path, but if the source file
+    //is nested in a directory that directory is also stored in the 'source_file' argument we pass
+    //here, I want to be able just use the name of the file to create it on the host system running
+    //the ducky script. 
+    //Essentially one of the problems is that this will write stuff like:
+    //$code = Get-Content .\test_files/short_test.txt -Raw
+    //But notice how this wouldn't work on windows since it uses forward slashes. 
+    //Need to handle this by checking OS type of where the command is run to generate ducky script. 
+    //
+    //EXAMPLE STRING for executing script on powershell windows: 
+    //"""
+    //STRINGLN $code = Get-Content .\\{YOUR_SOURCE_FILE} -Raw\n\
+    //DELAY 400\n\
+    //STRINGLN Invoke-Expression $code\n\
+    //"""
+    if os_type.to_lowercase() == "windows" {
+        execute_string.push_str(
+        "DELAY 100\n\
+        STRINGLN Set-ExecutionPolicy RemoteSigned -Scope CurrentUser\n\
+        DELAY 200\n\
+        ");
+
+    } else if os_type.to_lowercase() == "unix" {
+        execute_string.push_str(format!(
+            "STRINGLN chmod +x {}\n", source_file).as_str());
+        
+    } else {
+        return Err(HelpMessage::WrongArgOS)
+    }
+    Ok(execute_string)
+}
+
+//TODO make this a return statement with Result<String, HelpMessage>
 fn start_boilerplate(os_type: &str, source_file: &str) -> String {
     
     let mut os_start_string :String = String::new(); 
@@ -94,8 +145,7 @@ fn end_boilerplate(os_type: &str, source_file : &str) -> String {
     } else if os_type.to_lowercase() == "unix" {
         os_end_string.push_str(
             "CTRL d\n\
-            CTRL w\n\
-        ");
+            ");
 
     } else {
         println!("Something went wrong the boilerplate start function got the wrong OS type: {}", os_type);
@@ -105,10 +155,7 @@ fn end_boilerplate(os_type: &str, source_file : &str) -> String {
     os_end_string
 }
 
-fn print_type_of<T>(_: &T) {
-    println!("{}", std::any::type_name::<T>());
-}
-
+//TODO: add the ability to run this recursively if the user provides a directory with -d.
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
     where P: AsRef<Path>, {
     
@@ -116,8 +163,50 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
     Ok(io::BufReader::new(file).lines())
 }
 
+#[derive(Debug)]
+enum HelpMessage {
+    NotEnoughArgs,
+    WrongArgOS,
+    FileDoesNotExist,
+    MissingFlagO,
+}
+
+fn parse_args_advanced(args: &[String]) -> Result<(&str, bool, &str, &str), HelpMessage> {
+    
+    let mut iterator_args = args.into_iter();
+    let _ = iterator_args.next();
+
+    let mut os_target : &str = ""; 
+    let mut executable : bool = false;
+    let mut count : i32 = 0;
+    let mut source_path: &str = "";
+    let mut target_dest: &str = "";
+
+    //let x_flag = String::from("-x");
+    //let o_flag = String::from("-o");
+
+    while true {
+        let c_arg = iterator_args.next();
+        match c_arg {
+            Some(x) => 
+            match x.as_str() {
+                "-o" => match iterator_args.next() {
+                            Some(x) => os_target = x,
+                            None => return Err(HelpMessage::MissingFlagO)
+                        },
+                "-x" => executable = true,
+                s => {if count > 0 {target_dest = s} else {count += 1; source_path = s}}, 
+            },
+            None => break,
+        }
+    }
+    Ok((os_target, executable, source_path, target_dest))
+}
+
+//old function
 fn parse_args(args: &[String]) -> Result<(&str, &str, &str), String> {
     
+    println!("{:?}, ", args);
     match args.len() {
         // the first argument is automatic and is the binary path to where the script is running
         // from.
