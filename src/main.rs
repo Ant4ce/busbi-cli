7e83edb15e19f1568006e0e64f0a485d149adf02
Wessel_Weernink <wesselweernink@gmail.com>
2026-02-25 03:42:31 +0100

    Adds improvements to error handling and started the process of adding the ability to handle directories.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use std::{env, fs::File};
+use std::{env, fs::{File, read_dir}};
 use std::io::{self, BufRead, BufWriter, Write};
-use std::path::Path;
-use std::any::type_name;
+use std::path::{Path, PathBuf};
 
 fn main() -> io::Result<()> {
     let args : Vec<String> = env::args().collect();
@@ -16,7 +15,7 @@ fn main() -> io::Result<()> {
     //let desired_file_name : &str = &args[3];
 
     //let Ok((target_os, source_file, destination_file)) = parse_args(&args) else { panic!("Err: Wrong usage, try again.")};
-    let (target_os, execute, source_file, destination_file) = match parse_args_advanced(&args) {
+    let (target_os, execute, is_directory, source_file, destination_file, list_files) = match parse_args_advanced(&args) {
         Ok(x) => x,
         Err(x) => match x {
             HelpMessage::MissingFlagO => panic!("Got wrong or missing argument to -o flag."),
@@ -29,11 +28,11 @@ fn main() -> io::Result<()> {
     //1MB capacity for the buffer, TODO: make this an optional flag, which can be controlled.  
     let mut write_buf = BufWriter::with_capacity(1000000, new_file);
     
-    let start_boiler: String = start_boilerplate(target_os, source_file);
+    let start_boiler: String = start_boilerplate(target_os, &source_file);
     write_buf.write(start_boiler.as_bytes());
 
 
-    if let Ok(lines) = read_lines(source_file) {
+    if let Ok(lines) = read_lines(&source_file) {
         //print_type_of(&lines);
         for line in lines.map_while(Result::ok) {
             let mut mod_line : String = line.clone();
@@ -44,14 +43,14 @@ fn main() -> io::Result<()> {
 
         }
     } else {
-        println!("Err: File you specified doesn't exist or something else went wrong. Your file: {}", source_file);
+        println!("Err: File you specified doesn't exist or something else went wrong. Your file: {}", &source_file.display());
     }
 
-    let end_boiler: String = end_boilerplate(target_os, source_file);
+    let end_boiler: String = end_boilerplate(target_os, &source_file);
     write_buf.write(end_boiler.as_bytes());
 
     if execute == true {
-        let exec_string = match executable_boiler(target_os, source_file) {
+        let exec_string = match executable_boiler(target_os, &source_file) {
             Ok(x) => x,
             Err(x) => panic!("Got an error: {:?}", x),
         };
@@ -63,7 +62,55 @@ fn main() -> io::Result<()> {
 
 }
 
-fn executable_boiler(os_type: &str, source_file: &str) -> Result<String, HelpMessage> {
+//TODO: remove this code from the "main" function and just use this function, loop over it for when
+//handed directory. Then test it.
+fn file_handler(target_os: &str, execute: bool, source_file: PathBuf) -> Result<(), HelpMessage> {
+    //println!("the OS target is {target_os}, for target file {source_file} which will be turned into txt file with name {destination_file}");
+    let new_file = match File::create(&source_file) {
+        Ok(x) => x,
+        Err(e) => {eprintln!("got error: {}", e); return Err(HelpMessage::FailedToMakeFile)}
+    };
+    //1MB capacity for the buffer, TODO: make this an optional flag, which can be controlled.  
+    let mut write_buf = BufWriter::with_capacity(1000000, new_file);
+    
+    let start_boiler: String = start_boilerplate(target_os, &source_file);
+    write_buf.write(start_boiler.as_bytes());
+
+
+    if let Ok(lines) = read_lines(&source_file) {
+        //print_type_of(&lines);
+        for line in lines.map_while(Result::ok) {
+            let mut mod_line : String = line.clone();
+            //Changes to the line applied here before being written to the buffer.
+            mod_line.insert_str(0, "STRINGLN ");
+            mod_line.push_str("\n");
+            write_buf.write(mod_line.as_bytes());
+
+        }
+    } else {
+        println!("Err: File you specified doesn't exist or something else went wrong. Your file: {}", &source_file.display());
+    }
+
+    let end_boiler: String = end_boilerplate(target_os, &source_file);
+    write_buf.write(end_boiler.as_bytes());
+
+    if execute == true {
+        let exec_string = match executable_boiler(target_os, &source_file) {
+            Ok(x) => x,
+            Err(x) => panic!("Got an error: {:?}", x),
+        };
+        write_buf.write(exec_string.as_bytes());
+    }
+    //This pushes the contents of the buffer to the file. 
+    match write_buf.flush() {
+        Ok(x) => println!("Successfully wrote file."),
+        Err(e) => {eprintln!("got error: {}", e); return Err(HelpMessage::BufferFlushFailed)} 
+    };
+    Ok(())
+    
+}
+
+fn executable_boiler(os_type: &str, source_file: &PathBuf) -> Result<String, HelpMessage> {
     let mut execute_string : String = String::new();
 
     //TODO improve this here. Currently we just take the source file path, but if the source file
@@ -86,11 +133,17 @@ fn executable_boiler(os_type: &str, source_file: &str) -> Result<String, HelpMes
         "DELAY 100\n\
         STRINGLN Set-ExecutionPolicy RemoteSigned -Scope CurrentUser\n\
         DELAY 200\n\
+        STRINGLN $code = Get-Content .\\{YOUR_SOURCE_FILE} -Raw\n\
+        DELAY 400\n\
+        STRINGLN Invoke-Expression $code\n\
         ");
 
     } else if os_type.to_lowercase() == "unix" {
         execute_string.push_str(format!(
-            "STRINGLN chmod +x {}\n", source_file).as_str());
+            "STRINGLN chmod +x {}\n\
+            DELAY 100\n\
+            STRINGLN ./{}\n\
+            ", source_file.display(), source_file.display()).as_str());
         
     } else {
         return Err(HelpMessage::WrongArgOS)
@@ -99,7 +152,7 @@ fn executable_boiler(os_type: &str, source_file: &str) -> Result<String, HelpMes
 }
 
 //TODO make this a return statement with Result<String, HelpMessage>
-fn start_boilerplate(os_type: &str, source_file: &str) -> String {
+fn start_boilerplate(os_type: &str, source_file: &PathBuf) -> String {
     
     let mut os_start_string :String = String::new(); 
 
@@ -123,7 +176,7 @@ fn start_boilerplate(os_type: &str, source_file: &str) -> String {
             ENTER\n\
             DELAY 400\n\
             STRINGLN cat > {}\n\
-        ", source_file).as_str());
+        ", source_file.display()).as_str());
     } else {
         println!("Something went wrong the boilerplate start function got the wrong OS type: {}", os_type);
         //TODO: proper error handling.
@@ -134,13 +187,13 @@ fn start_boilerplate(os_type: &str, source_file: &str) -> String {
 
 }
 
-fn end_boilerplate(os_type: &str, source_file : &str) -> String {
+fn end_boilerplate(os_type: &str, source_file : &PathBuf) -> String {
     let mut os_end_string: String = String::new();
     if os_type.to_lowercase() == "windows" {
         os_end_string.push_str(format!(
             "STRINGLN \"@\n\
             STRINGLN Set-Content -Path $HOME\\{} -Value $content\n\
-        ", source_file).as_str());
+        ", source_file.display()).as_str());
         
     } else if os_type.to_lowercase() == "unix" {
         os_end_string.push_str(
@@ -169,21 +222,30 @@ enum HelpMessage {
     WrongArgOS,
     FileDoesNotExist,
     MissingFlagO,
+    DirectoryDoesNotExist,
+    FailedToGetFile,
+    FailedToMakeFile,
+    BufferFlushFailed,
 }
 
-fn parse_args_advanced(args: &[String]) -> Result<(&str, bool, &str, &str), HelpMessage> {
+fn parse_args_advanced(args: &[String]) -> Result<(&str, bool, bool, PathBuf, &str, Vec<PathBuf>), HelpMessage> {
     
     let mut iterator_args = args.into_iter();
     let _ = iterator_args.next();
 
+    //TODO: Set this to 'Windows' or 'Unix' to make it default.
     let mut os_target : &str = ""; 
     let mut executable : bool = false;
+    let mut is_directory : bool = false;
+    let mut list_files : Vec<PathBuf> = Vec::new();
     let mut count : i32 = 0;
-    let mut source_path: &str = "";
+    //let mut source_path: &str = "";
+    let mut source_path: std::path::PathBuf = PathBuf::new();
     let mut target_dest: &str = "";
 
-    //let x_flag = String::from("-x");
-    //let o_flag = String::from("-o");
+    if args.len() < 3 {
+        return Err(HelpMessage::NotEnoughArgs)
+    }
 
     while true {
         let c_arg = iterator_args.next();
@@ -195,13 +257,42 @@ fn parse_args_advanced(args: &[String]) -> Result<(&str, bool, &str, &str), Help
                             None => return Err(HelpMessage::MissingFlagO)
                         },
                 "-x" => executable = true,
-                s => {if count > 0 {target_dest = s} else {count += 1; source_path = s}}, 
+                //TODO: this has to be the last option given. Mention in help.
+                "-d" => match iterator_args.next() {
+                            Some(x) => {
+                                is_directory = true;
+                                source_path = PathBuf::from(x);
+                                if source_path.is_dir() != true {
+                                    eprintln!("Argument provided is not a directory: {}", source_path.display());
+                                    return Err(HelpMessage::DirectoryDoesNotExist)
+                                }
+                                let mut directory_iterator = match read_dir(&source_path) {
+                                    Ok(x) => x,
+                                    Err(e) => {println!("got error: {:?}", e); return Err(HelpMessage::DirectoryDoesNotExist)},
+                                };
+                                while true {
+                                    let current_entry = directory_iterator.next();
+                                    match current_entry {
+                                        Some(x) => match x {
+                                            Ok(y) => list_files.push(y.path()),
+                                            Err(e) => {eprintln!("got error: {}", e); return Err(HelpMessage::FailedToGetFile)},
+                                        },
+                                        None => break,
+                                    }
+                                }
+                                return Ok((os_target, executable, is_directory, source_path, target_dest, list_files))
+                                
+                            },
+                            None => return Err(HelpMessage::DirectoryDoesNotExist)
+                }, 
+                s => {if count > 0 {target_dest = s} else {count += 1; source_path = PathBuf::from(s)}}, 
             },
             None => break,
         }
     }
-    Ok((os_target, executable, source_path, target_dest))
+    Ok((os_target, executable, is_directory, source_path, target_dest, list_files))
 }
+
 
 //old function
 fn parse_args(args: &[String]) -> Result<(&str, &str, &str), String> {
